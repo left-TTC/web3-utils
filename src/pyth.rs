ce754074fc3f4e123f50f4b80525ee52702c2c7c
left-TTC <leftear1321@gmail.com>
2025-09-11 12:51:20 +0800

    compile test

--- src/pyth.rs
+++ src/pyth.rs
@@ -1,16 +1,13 @@
 
 use solana_program::{
-    program_error::ProgramError, msg,
-    pubkey::Pubkey,
+    clock::Clock, msg, program_error::ProgramError, pubkey::Pubkey, sysvar::Sysvar
 };
 use pyth_sdk_solana::{
     state::{
-        load_mapping_account, load_price_account, load_product_account, CorpAction, PriceStatus,
-        PriceType,
+        load_price_account, SolanaPriceAccount
     },
     Price,
 };
-
 
 
 pub fn get_oracle_price_fp32(
@@ -18,24 +15,41 @@ pub fn get_oracle_price_fp32(
     base_decimals: u8,
     quote_decimals: u8,
 ) -> Result<u64, ProgramError> {
-    let price_account = load_price_account(account_data)?;
-    let Price { price, expo, .. } = price_account
-        .to_price_feed(&Pubkey::default())
-        .get_current_price()
-        .ok_or_else(|| {
-            msg!("Cannot parse pyth price, information unavailable.");
-            ProgramError::InvalidAccountData
-        })?;
-    let price = if expo > 0 {
-        ((price as u128) << 32) * 10u128.pow(expo as u32)
+    let price_account: &SolanaPriceAccount = load_price_account(account_data)?;
+    let price_feed = price_account.to_price_feed(&Pubkey::default());
+
+    let currentg_time = Clock::get().unwrap().unix_timestamp;
+    let Price { price, expo, .. } = price_feed
+    .get_price_no_older_than(currentg_time, 60)
+    .ok_or_else(|| {
+        msg!("Cannot parse pyth price, information unavailable.");
+        ProgramError::InvalidAccountData
+    })?;
+
+    let scaling = 10u128
+        .checked_pow(expo.unsigned_abs() as u32)
+        .ok_or(ProgramError::InvalidArgument)?;
+
+    let price_fp32 = if expo >= 0 {
+        ((price as i128) as u128)
+            .checked_shl(32)
+            .and_then(|v| v.checked_mul(scaling))
+            .ok_or(ProgramError::InvalidArgument)?
     } else {
-        ((price as u128) << 32) / 10u128.pow((-expo) as u32)
+        ((price as i128) as u128)
+            .checked_shl(32)
+            .and_then(|v| v.checked_div(scaling))
+            .ok_or(ProgramError::InvalidArgument)?
     };
 
-    let corrected_price =
-        (price * 10u128.pow(quote_decimals as u32)) / 10u128.pow(base_decimals as u32);
+    let corrected_price = price_fp32
+        .checked_mul(10u128.pow(quote_decimals as u32))
+        .and_then(|v| v.checked_div(10u128.pow(base_decimals as u32)))
+        .ok_or(ProgramError::InvalidArgument)?;
 
-    let final_price = corrected_price.try_into().unwrap();
+    let final_price: u64 = corrected_price
+        .try_into()
+        .map_err(|_| ProgramError::InvalidArgument)?;
 
     msg!("Pyth FP32 price value: {:?}", final_price);
 
