d6f4fe0be9ac7cd21d55590d9e2708e54b3eb833
left-TTC <leftear1321@gmail.com>
2025-09-12 20:47:03 +0800

    add instructionaccounts

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,9 @@ pub mod price_update;
 pub mod borsh_size;
 pub mod accounts;
 pub mod wrapped_pod;
+
+
+pub use web3_macros::{
+    compute_hashv as compute_record_hash, compute_record_hash_v2, declare_id_with_central_state,
+    BorshSize, InstructionsAccount, WrappedPod, WrappedPodMut,
+};
